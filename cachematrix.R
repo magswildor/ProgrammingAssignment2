4c2c407e2d851be9a217f3deabe6809830f6b5c5
magswildor <mwildor@its.jnj.com>
2017-02-20 07:28:15 +0000

    Programmning Project Submission
    Constructor Function - inverse of matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+# create a constructor fn which gets the inverse of a matrix.  the inverse is 
+# the objective functon
 
-## Write a short comment describing this function
+
+# creates special 'vector' - a list containing a fn to set & get value of vec
+# set the value of inverse, get value of inverse
 
 makeCacheMatrix <- function(x = matrix()) {
+  s <- NULL
+  set <- function(y) {
+    x <<- y
+    s <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(solve) s <<- solve
+  getinverse <- function() s
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+}
 
+makeCacheMatrix <- function(x = matrix()) {
+  
 }
 
 
-## Write a short comment describing this function
+# This function calculates the inverse of the matrix if it has not already been
+# calculated and stored in the cache
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  s <- x$getinverse()
+  if(!is.null(s)) {
+    message("getting cached data")
+    return(s)
+  }
+  data <- x$get()
+  s <- solve(data, ...)
+  x$setinverse(s)
+  s
 }
+
+
+
+
